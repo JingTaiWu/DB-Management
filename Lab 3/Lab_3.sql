ee2c898f1178191689d6339122c358a17dba9b84
Jing Tai Wu <jing.wu1@marist.edu>
2014-02-06 14:32:21 -0600

    Lab 3 completed 2/6/2014

--- Lab 3/Lab_3.sql
+++ Lab 3/Lab_3.sql
@@ -3,7 +3,41 @@ select name, city
 from agents
 where name = 'Smith';
 
---Nunber 2
+--Number 2
 select pid, name, quantity
 from products
 where priceusd > 1.25;
+
+--Number 3
+select ordno, aid
+from orders;
+
+--Number 4
+select name, city
+from customers
+where city = 'Dallas';
+
+--Number 5
+select name
+from agents
+where city != 'New York' and city != 'Newark';
+
+--Number 6
+select *
+from products
+where city != 'New York' and city != 'Newark' and priceusd >= 1;
+
+--Number 7
+select *
+from orders
+where mon = 'jan' or mon = 'mar';
+
+--Number 8
+select *
+from orders
+where mon = 'feb' and dollars < 100;
+
+--Number 9
+select ordno
+from orders
+where cid = 'c001';
