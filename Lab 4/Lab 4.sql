1db9c36c29540bc3a573940297eec6499aaa9a91
Jing Tai Wu <jing.wu1@marist.edu>
2014-02-08 16:44:28 -0600

    Lab 4 completed 2/8/2014

---
+++ Lab 4/Lab 4.sql
@@ -0,0 +1,63 @@
+﻿--Jinig Tai Wu
+--Lab 4
+
+
+--Get the Cities of agents booking an order for customer Basics
+select city
+from agents
+where aid in (select aid 
+	      from orders
+	      where cid in (select cid
+			    from customers
+			    where name = 'Basics'));
+
+--Get the pids of products ordered through any agent who makes at least one order
+--for a customer in Kyoto.
+select distinct pid
+from Orders
+where cid in (select cid
+	      from customers
+	      where city = 'Kyoto');
+
+--Find the cids and names of customers who never placed an order through agent a03
+select cid, name
+from customers
+where cid in (select cid
+	      from orders
+	      where cid != 'a03');
+
+--Get the cids and names of customers who ordered both product p01 and p07
+select cid, name
+from customers
+where cid in (select cid
+              from orders
+              where pid = 'p01'
+		intersect select cid
+			  from orders
+			  where pid = 'p07');
+
+--Get the pids of products ordered by any customers who ever placed an order
+--through agent a03
+select pid
+from orders
+where aid in (select aid
+              from orders
+              where aid = 'a03');
+
+--Get the names and discounts of all customers who ever placed an oder through
+--agents in Dallas or Duluth
+select name, discount
+from customers
+where cid in (select cid
+	      from orders
+	      where aid in (select aid
+			    from agents
+			    where city = 'Dallas' OR city = 'Duluth'));
+
+--Find all customers who have the same discount as that of any customers in Dallas
+--or Kyoto
+select *
+from customers
+where discount in (select discount
+	           from customers
+	           where city = 'Dallas' OR city = 'Kyoto');
