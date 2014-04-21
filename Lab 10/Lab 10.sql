611bec00342d1bde8e21ffbacbc0db0b346447f6
Jing Tai Wu <jingtai1993@gmail.com>
2014-04-21 19:40:15 -0400

    Finish Lab 10

---
+++ Lab 10/Lab 10.sql
@@ -0,0 +1,42 @@
+﻿--Jing Tai Wu
+--Last Lab
+
+--function PreReqsfor
+create or replace function PreReqsFor(integer, REFCURSOR) returns REFCURSOR as
+$$
+declare
+	courseNum_Req integer := $1;
+	resultset   REFCURSOR := $2;
+begin
+	open resultset for
+		select preReqNum
+		from Prerequisites
+		where courseNum = courseNum_Req;
+	return resultset;
+end;
+$$
+language plpgsql; 
+
+--test it out
+select PreReqsFor(499, 'results');
+Fetch all from results;
+
+--Functions IsPReReqsFor
+create or replace function IsPreReqFor(integer, refcursor) returns refcursor as
+$$
+declare
+	preReq	integer := $1;
+	resultset refcursor := $2;
+begin
+	open resultset for
+		select courseNum
+		from Prerequisites
+		where preReqNum = preReq;
+	return resultset;
+end;
+$$
+language plpgsql;
+
+--test it out
+select IsPreReqFor(120, 'result');
+Fetch all from result;
